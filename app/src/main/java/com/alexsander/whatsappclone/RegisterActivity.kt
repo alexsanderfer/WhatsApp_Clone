3f8b6c4e71fab41cd8e51dbe793c3762561b4480
Alexsander Fernandes <afgouveia90@gmail.com>
2024-02-22 02:46:52 +0000

    .

--- app/src/main/java/com/alexsander/whatsappclone/RegisterActivity.kt
+++ app/src/main/java/com/alexsander/whatsappclone/RegisterActivity.kt
@@ -2,10 +2,22 @@ package com.alexsander.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alexsander.whatsappclone.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityRegisterBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
+        initializeToolbar()
+    }
+
+    private fun initializeToolbar() {
+        val toolbar = binding.includeToolbar.mMainToolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Faça o seu cadastro"
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
